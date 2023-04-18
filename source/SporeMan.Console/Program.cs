b08a406d1ed38d12fffaa07582426f58a0880e33
nightblade9 <nightbladecodes@gmail.com>
2023-04-18 19:22:30 -0400

    Intro, SpectreConsole, VSCode launch tasks

--- source/SporeMan.Console/Program.cs
+++ source/SporeMan.Console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+namespace SporeMan;
+
+class Program {
+    public static void Main(string[] args) {
+        AnsiConsole.MarkupLine("[#080]Spore[/]Man");
+        var input = Console.ReadKey(true);
+        Console.WriteLine($"Bye, {input.KeyChar}!");
+        Thread.Sleep(1000);
+    }
+}
