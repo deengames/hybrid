ae5651b067f5091007e37128274e4f60d71816cc
nightblade9 <nightbladecodes@gmail.com>
2023-04-18 19:47:35 -0400

    Proof of concept: async audio + done notification

--- source/SporeMan.Console/Program.cs
+++ source/SporeMan.Console/Program.cs
@@ -6,6 +6,7 @@ namespace SporeMan.Console;
 class Program {
     public static void Main(string[] args) {
         AudioPlayer.Play(Path.Join("assets", "audio", "sfx", "random.wav"));
+        AudioPlayer.OnPlayDone += (file) => System.Console.WriteLine($"Done {file}");
         AnsiConsole.MarkupLine("[#080]Spore[/]Man");
         var input = System.Console.ReadKey(true);
         System.Console.WriteLine($"Bye, {input.KeyChar}!");
--- source/SporeMan.Console/io/AudioPlayer.cs
+++ source/SporeMan.Console/io/AudioPlayer.cs
@@ -3,16 +3,22 @@ using NAudio.Wave;
 namespace SporeMan.Console.IO;
 
 static class AudioPlayer {
+
+    public static Action<string> OnPlayDone;
+
     public static void Play(string sound) {
-        using(var audioFile = new AudioFileReader(sound)) {
-            using(var outputDevice = new WaveOutEvent()) {
-                outputDevice.Init(audioFile);
-                outputDevice.Play();
-                while (outputDevice.PlaybackState == PlaybackState.Playing)
-                {
-                    Thread.Sleep(1000);
+        new Thread(() => {
+            using(var audioFile = new AudioFileReader(sound)) {
+                using(var outputDevice = new WaveOutEvent()) {
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                    OnPlayDone?.Invoke(sound);
                 }
             }
-        }
+        }).Start();
     }
 }
