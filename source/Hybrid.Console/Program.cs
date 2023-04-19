a7bda9ac8f7b1bb88520ab801687d7545c8f3e29
nightblade9 <nightbladecodes@gmail.com>
2023-04-18 23:54:55 -0400

    Rename title screen, use internal console

--- source/Hybrid.Console/Program.cs
+++ source/Hybrid.Console/Program.cs
@@ -5,7 +5,7 @@ namespace Hybrid.Console;
 
 class Program {
     public static void Main(string[] args) {
-        AnsiConsole.MarkupLine("[#0f8]Spore[/]Man");
+        AnsiConsole.MarkupLine("[#0f8]Hyb[/]rid");
         AnsiConsole.MarkupLine("Press any key to start a new game.");
         System.Console.ReadKey();
     }
