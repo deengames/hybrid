1efaec941f594e6def240c5d04c30f0d3f5fb03b
nightblade9 <nightbladecodes@gmail.com>
2023-04-21 01:05:59 -0400

    Crude skill selection, no storage yet.

--- source/Hybrid.Game/Character/SkillSelectScene.cs
+++ source/Hybrid.Game/Character/SkillSelectScene.cs
@@ -1,4 +1,3 @@
-using Hybrid.Core.Character;
 using Hybrid.Core.Data.Skills;
 using Hybrid.Game.Interfaces;
 using Hybrid.Game.IO;
@@ -8,30 +7,55 @@ namespace Hybrid.Game.Character;
 
 class SkillSelectScene : IScene
 {
+    // Order matters
+    private readonly IEnumerable<SkillData> unlearnedSkills;
+
+    public SkillSelectScene(string[] learnedSkills)
+    {
+        unlearnedSkills = PlayerSkillsData.GetUnlearnedSkills(learnedSkills);
+    }
+
     public void Show()
     {
         var player = Game.Instance.Player;
 
         ShowIntro(player.SkillPoints);
         ShowCurrentSkills(player.Skills);
-        ListUnlearnedSkills(player.Skills);
+        ListUnlearnedSkills();
+        var toLearn = AskWhichSkillToLearn();
+        AnsiConsole.Write($"YOU LEARNED: {toLearn.Name}!!!!");
         System.Console.ReadKey(true);
         Game.Instance.End();
     }
 
-    private void ListUnlearnedSkills(string[] skills)
+    private SkillData AskWhichSkillToLearn()
     {
-        var unlearnedSkills = PlayerSkillsData.GetUnlearnedSkills(skills);
+        var input = "";
+        int selection = 0;
+        while (selection <= 0 || selection > unlearnedSkills.Count())
+        {
+            AnsiConsole.Write("Enter the number of the skill to learn: ");
+            input = Console.ReadLine();
+            int.TryParse(input, System.Globalization.NumberStyles.Integer, null, out selection);
+        }
+
+        return unlearnedSkills.ElementAt(selection - 1);
+    }
+
+    private void ListUnlearnedSkills()
+    {
+        AnsiConsole.MarkupLine($"[{Colours.ThemeHighlight}]Unlearned skills:[/]\n");
         var previousSpecies = "";
 
-        foreach (var unlearned in unlearnedSkills)
+        for (var i = 0; i < unlearnedSkills.Count(); i++)
         {
+            var unlearned = unlearnedSkills.ElementAt(i);
             if (unlearned.Species != previousSpecies)
             {
                 previousSpecies = unlearned.Species;
                 AnsiConsole.MarkupLine($"Species: [{Colours.ThemeHighlight}]{unlearned.Species}[/]");
             }
-            AnsiConsole.MarkupLine($"  [{Colours.ThemeHighlight}]{unlearned.Name}[/] ({unlearned.LearningCost} points): {unlearned.Description}. [{Colours.ThemeHighlight}]{unlearned.Effect}[/].");
+            AnsiConsole.MarkupLine($"  {i + 1}: [{Colours.ThemeHighlight}]{unlearned.Name}[/] ({unlearned.LearningCost} points): {unlearned.Description}. [{Colours.ThemeHighlight}]{unlearned.Effect}[/].");
         }
     }
 
@@ -42,6 +66,6 @@ class SkillSelectScene : IScene
 
     private void ShowCurrentSkills(string[] skills)
     {
-        AnsiConsole.MarkupLine($"[{Colours.ThemeHighlight}]Current skills[/] ({skills.Length}): {string.Join(", ", skills)}");
+        AnsiConsole.MarkupLine($"[{Colours.ThemeHighlight}]Current skills[/] ({skills.Length}): {string.Join(", ", skills)}\n");
     }
 }
