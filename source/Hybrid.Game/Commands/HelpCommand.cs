67cf73acb195b1180bb31d257212239752100057
nightblade9 <nightbladecodes@gmail.com>
2023-04-26 16:35:28 -0400

    Update help to include omre commands.

--- source/Hybrid.Game/Commands/HelpCommand.cs
+++ source/Hybrid.Game/Commands/HelpCommand.cs
@@ -7,6 +7,6 @@ class HelpCommand : ICommand
 {
     public void Run()
     {
-        AnsiConsole.MarkupLine($"Type [{Colours.ThemeHighlight}]n, e, s, or w[/] to move north, east, south, or west; type [{Colours.ThemeHighlight}]l[/] to look around, and [{Colours.ThemeHighlight}]f[/] to fight automatically.");
+        AnsiConsole.MarkupLine($"Type [{Colours.ThemeHighlight}]n, e, s, or w[/] to move north, east, south, or west; type [{Colours.ThemeHighlight}]l[/] to look around, and [{Colours.ThemeHighlight}]f[/] to fight automatically. To check what skills you can learn, type [{Colours.ThemeHighlight}]c[/] or [{Colours.ThemeHighlight}]skill[/]. When you find the crater down to the next floor, type [{Colours.ThemeHighlight}]d[/] to descend.");
     }
 }
