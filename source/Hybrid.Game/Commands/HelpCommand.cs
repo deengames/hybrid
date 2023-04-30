8de004b468a5929b9f93b7918f6bf3c94726e350
nightblade9 <nightbladecodes@gmail.com>
2023-04-29 23:13:14 -0400

    Help includes TIPS to WIN

--- source/Hybrid.Game/Commands/HelpCommand.cs
+++ source/Hybrid.Game/Commands/HelpCommand.cs
@@ -8,5 +8,13 @@ class HelpCommand : ICommand
     public void Run()
     {
         AnsiConsole.MarkupLine($"Type [{Colours.ThemeHighlight}]n, e, s, or w[/] to move north, east, south, or west; type [{Colours.ThemeHighlight}]l[/] to look around, and [{Colours.ThemeHighlight}]f[/] to fight automatically. To check what skills you can learn, type [{Colours.ThemeHighlight}]c[/] or [{Colours.ThemeHighlight}]skill[/]. When you find the crater down to the next floor, type [{Colours.ThemeHighlight}]d[/] to descend.");
+
+        AnsiConsole.WriteLine();
+
+        AnsiConsole.MarkupLine($"[{Colours.ThemeHighlight}]Tips:[/]");
+        AnsiConsole.WriteLine("  1) You return to full health before each battle.");
+        AnsiConsole.WriteLine("  2) Choose skills carefully, they make a big difference.");
+        AnsiConsole.WriteLine("  3) You gain experience points and level up when you kill enough monsters.");
+        AnsiConsole.WriteLine("  4) You also gain a level every time you descend down a floor.");
     }
 }
